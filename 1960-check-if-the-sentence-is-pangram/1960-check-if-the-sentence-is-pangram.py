9080e9c691421bca29f111e1e950dc93f14823bd
Hari Vamsi <harivamsiyadavalli@gmail.com>
2023-11-03 05:29:47 -0700

    Time: 41 ms (40.39%), Space: 16.3 MB (37.45%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
@@ -1,13 +1,13 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        # Brute Force TC: O(N) SC: O(1)
+        # Brute Force TC: O(N^2) SC: O(1)
         # check = 'qwertyuiopasdfghjklzxcvbnm'
         # for letter in check:
         #     if letter not in sentence:
         #         return False
         # return True
 
-        # 
+        # Optimized Solution TC: O(N) SC: O(N)
         dict_1 = {}
         for s in sentence:
             dict_1[s] = 1
