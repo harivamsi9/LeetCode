3c413dd1fe6e06f6bb4fd301653b29a39233235b
Hari Vamsi <harivamsiyadavalli@gmail.com>
2023-11-03 04:34:27 -0700

    Time: 43 ms (64.33%), Space: 16.2 MB (42.37%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
@@ -14,16 +14,19 @@ class Solution:
         # return ans
 
         ## Space Optimized:: TC: O(2N) SP: O(1)
-        # Loop once, and get the max from the candies list.
-        # In 2nd loop, check if candies[i]+extraCandies >= max
-        # and update
-        max_c = max(candies)
-        for i in range(len(candies)):
-            if candies[i] + extraCandies >= max_c:
-                candies[i] = True
-            else:
-                candies[i] = False
-        return candies
+        ## Loop once, and get the max from the candies list.
+        ## In 2nd loop, check if candies[i]+extraCandies >= max
+        ## and update
+        # max_c = max(candies)
+        # for i in range(len(candies)):
+        #     if candies[i] + extraCandies >= max_c:
+        #         candies[i] = True
+        #     else:
+        #         candies[i] = False
+        # return candies
+
+        check=max(candies)-extraCandies
+        return [k>=check for k in candies]
 
 
 
