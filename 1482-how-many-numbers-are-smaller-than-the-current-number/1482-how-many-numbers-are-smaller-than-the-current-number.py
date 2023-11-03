c771f1c8c83e52675819dbcb299240bb9ba2cb00
Hari Vamsi <harivamsiyadavalli@gmail.com>
2023-11-03 05:09:50 -0700

    Time: 57 ms (85.12%), Space: 16.2 MB (70.95%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,13 +1,31 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        # brute Force TC: O(N^2) SC: O(N)
-        ans = []
-        for i in range(0,len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if i != j and nums[j] < nums[i]:
-                    count += 1
-            ans.append(count)
-        return ans
+        # ## brute Force TC: O(N^2) SC: O(N)
+        # ans = []
+        # for i in range(0,len(nums)):
+        #     count = 0
+        #     for j in range(len(nums)):
+        #         if i != j and nums[j] < nums[i]:
+        #             count += 1
+        #     ans.append(count)
+        # return ans
+
+        ## Approach 1: Time Optimization
+        # sort the array
+        ans = nums.copy()
+        # ans = list(set(ans))
+        ans.sort()
+        dict_1 = {}
+        for j in range(0,len(ans)):
+            if ans[j] not in dict_1:
+                dict_1[ans[j]] = j 
+        print(dict_1)
+
+        for i in range(len(nums)):
+            nums[i] = dict_1[nums[i]]
+        return nums
+
+
+
 
                     
