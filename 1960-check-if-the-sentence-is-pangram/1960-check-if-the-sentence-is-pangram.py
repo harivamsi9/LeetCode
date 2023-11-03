848b2fc36f7d1cc15da3d696da56c516f6caf0b0
Hari Vamsi <harivamsiyadavalli@gmail.com>
2023-11-03 05:27:43 -0700

    Time: 34 ms (83.85%), Space: 16 MB (94.23%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
@@ -1,9 +1,16 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        check = 'qwertyuiopasdfghjklzxcvbnm'
+        # Brute Force TC: O(N) SC: O(1)
+        # check = 'qwertyuiopasdfghjklzxcvbnm'
+        # for letter in check:
+        #     if letter not in sentence:
+        #         return False
+        # return True
 
-        for letter in check:
-            if letter not in sentence:
-                return False
-
+        # 
+        dict_1 = {}
+        for s in sentence:
+            dict_1[s] = 1
+        if len(dict_1.keys()) != 26:
+            return False
         return True
