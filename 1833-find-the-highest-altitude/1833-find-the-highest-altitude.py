25691c1edb125e9897222be233e6d6971acc6285
Hari Vamsi <harivamsiyadavalli@gmail.com>
2023-11-03 16:46:47 -0700

    Time: 40 ms (70.55%), Space: 16.4 MB (7.42%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
@@ -1,13 +1,21 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
+        # ## Brute Force TC: O(N) SC: (N)
+        # ans = [0]
+        # max_sum = 0
+        # for i in range(len(gain)):
+        #     new = ans[-1] + gain[i]
+        #     max_sum = max(max_sum, new)
+        #     ans.append(new)
+        # return max_sum
 
-        ans = [0]
-        max_sum = 0
-        for i in range(len(gain)):
-            new = ans[-1] + gain[i]
-            max_sum = max(max_sum, new)
-            ans.append(new)
-
+        # Space Optimized
+        max_sum = max(0,gain[0])
+        for i in range(1, len(gain)):
+            gain[i] = gain[i] + gain[i-1]
+            max_sum = max(gain[i], max_sum)
         return max_sum
+
+
 
         
