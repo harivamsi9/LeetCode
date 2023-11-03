e183ce86d93df59eba51f0c6120257409ba8956e
Hari Vamsi <harivamsiyadavalli@gmail.com>
2023-11-03 04:44:37 -0700

    Time: 34 ms (90.48%), Space: 16.2 MB (36.33%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -1,11 +1,26 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        ## Brute Force
+        ## Brute Force TC: O(N^2) SC: O(1)
+        # count = 0
+        # for j in range(1,len(nums)):
+        #     for i in range(j):
+        #         if nums[i] == nums[j]:
+        #             count += 1
+        # return count
+
+        # optimized 
+        counter = {}
+        for i in nums:
+            if i in counter:
+                counter[i] += 1
+            else:
+                counter[i] = 1
         count = 0
-        for j in range(1,len(nums)):
-            for i in range(j):
-                if nums[i] == nums[j]:
-                    count += 1
+        for i in counter.keys():
+            n = counter[i]
+            count += n*(n-1)//2
         return count
+
+
                     
         
