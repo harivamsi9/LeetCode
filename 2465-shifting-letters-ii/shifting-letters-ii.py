08697905679ea6d4142d096cbd650065fcbfa94a
Hari Vamsi <harivamsiyadavalli@gmail.com>
2025-01-04 22:10:57 -0800

    Time: 83 ms (22.19%) | Memory: 42 MB (7.60%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -1,21 +1,27 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        ## Difference Array Approch
+        ## Time Complexity - O(N)
+        
         diffArr = [0]*len(s)
+
         for q in shifts:
             l,r,x = q
             if x == 0:
                 x = -1
             else:
                 x = 1
+        
             diffArr[l] += x
             if r+1 < len(s):
                 diffArr[r+1] -= x
+
         ps = [0]*len(s)
         ps[0] = diffArr[0]
+
         for i in range(1,len(s)):
             ps[i] += diffArr[i] + ps[i-1]
-        print(diffArr)
-        print(ps)
+
         ans = ''
         for i in range(len(s)):
             newChar_ord = ord('a') + ((ord(s[i]) - ord('a') + ps[i]) % 26)
