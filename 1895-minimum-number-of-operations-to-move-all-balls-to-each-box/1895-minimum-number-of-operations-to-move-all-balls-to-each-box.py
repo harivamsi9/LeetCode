afa22d811b2df8aee30d5e76f58d4ce649fc865b
Hari Vamsi <harivamsiyadavalli@gmail.com>
2025-01-06 00:45:39 -0800

    Time: 7 ms (89.2%), Space: 18.2 MB (16.05%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,8 +1,10 @@
 class Solution:
     def minOperations(self, arr: str) -> List[int]:
+        # Prefix Sum Method 
+        # TC - O(n), SC - O(n)
         arr = [int(i) for i in arr]
 
-        ps = [0]*len(arr)
+        ps = [0] * len(arr)
         ss = [0] * len(arr)
         ans = [0] *len(arr)
 
@@ -11,14 +13,12 @@ class Solution:
         for i in range(1, len(arr)):
             ps[i] += ps[i-1] + c
             c += arr[i]
-        print(ps)
 
         c = arr[-1]
         ss[-1] = 0
         for i in range(len(arr)-1 -1,-1,-1):
             ss[i] += ss[i+1] + c
             c += arr[i]
-        print(ss)
 
         for i in range(len(arr)):
             ans[i] = ps[i] + ss[i]
