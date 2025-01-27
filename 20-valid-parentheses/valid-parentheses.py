0761017616b3b725d069eb0e02a8e1957ad2a864
Hari Vamsi <harivamsiyadavalli@gmail.com>
2025-01-27 13:59:45 -0800

    Time: 4 ms (9.09%) | Memory: 17.7 MB (57.99%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        d = {
+            ")" : "(", 
+            "}" : "{", 
+            "]" : "["
+        }
+        stack = []
+
+        for i in s:
+            if i in d:
+                if len(stack) > 0 and stack[-1] == d[i]:
+                    stack.pop()
+                else:
+                    stack.append(i)
+            else:
+                stack.append(i)
+        if stack: return False
+        return True
+
+
+        
