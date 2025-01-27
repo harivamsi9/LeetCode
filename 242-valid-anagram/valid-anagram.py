df9807bd2d01303ae10995281972fa552a8ba829
Hari Vamsi <harivamsiyadavalli@gmail.com>
2025-01-27 04:40:17 -0800

    Time: 11 ms (74.13%) | Memory: 17.8 MB (52.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,6 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sc = Counter(s)
-        tc = Counter(t)
-        return sc == tc
+        ## TC - O(N); SC - O(1)
+        if len(s) != len(t): return False
+        count = [0]*26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        for c in count:
+            if c != 0: return False
+        return True
+            
         
