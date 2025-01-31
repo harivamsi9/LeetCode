ad96fa14aa13ef6b7bb3866c00aad2a5d7872d81
Hari Vamsi <harivamsiyadavalli@gmail.com>
2025-01-30 21:51:09 -0800

    Time: 408 ms (44.64%) | Memory: 17.7 MB (78.69%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,35 +1,36 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        max_count = 0
-        ans = ''
-        for i in range(len(s)):
-            j,k = i,i
-            count = 0
-            while j >=0 and k <= len(s)-1 and s[j] == s[k]:
-                count += 1
-                j -= 1
-                k += 1
-            
-            if count > max_count: 
-                ans = s[j+1:k]
-                max_count = max(count, max_count)
+        # s = "cbbd"
+        maxCount = float('-inf')
+        ans1 = ""
 
-        ans2 = ''
-        max_count2 = 0
-        for i in range(len(s)):
-            j,k = i,i+1
-            count = 0
-            while j >=0 and k <= len(s)-1 and s[j] == s[k]:
-                count += 1
+        for i in range(len(s)): # "cbbd"
+            count1 = 0
+            # odd length
+            j, k = i, i
+            while 0 <= j < len(s) and 0 <= k < len(s) and s[j] == s[k]:
+                count1 += 1
                 j -= 1
                 k += 1
             
-            if count > max_count2: 
+            if count1 > maxCount:
+                maxCount = count1  # This is fine, no need for max(maxCount, count1)
+                ans1 = s[j+1:k]
+
+        maxCount2 = float('-inf')
+        ans2 = ""
+        for i in range(len(s)):
+            count2 = 0
+            # even length
+            j, k = i, i + 1
+            while 0 <= j < len(s) and 0 <= k < len(s) and s[j] == s[k]:
+                count2 += 1
+                j -= 1
+                k += 1
+            
+            if count2 > maxCount2:
+                maxCount2 = count2  # Same here, just use count2
                 ans2 = s[j+1:k]
-                max_count2 = max(count, max_count2)
-            
-        print(ans, ans2)
+        print(ans1, ans2)
 
-        return ans if len(ans) > len(ans2) else ans2
-            
-        
+        return ans1 if len(ans1) > len(ans2) else ans2
