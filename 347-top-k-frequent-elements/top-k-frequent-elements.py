b6055bf9b7089efe727b2fe7f4e36b7cbef2df4f
Hari Vamsi <harivamsiyadavalli@gmail.com>
2026-01-24 13:52:11 -0800

    Time: 0 ms (100.00%) | Memory: 22.9 MB (38.62%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,25 +1,33 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         c = Counter(nums)
-        ans = []
-        cc = {} # freq -> set(unique nums)
-        for kk,v in c.items():
-            if v in cc:
-                cc[v].add(kk)
-            else:
-                cc[v]=set()
-                cc[v].add(kk)
 
-        max_k = len(nums)
-        while (max_k > 0 and k > 0):
-            if max_k in cc and k>0:
-                arr = cc[max_k]
-                for a in arr:
-                    ans.append(a)
-                    k -= 1
-            max_k -= 1
+        freq_map = defaultdict(list) # freq --> (unique) num mapping
 
-        return ans
+        for ke,v in c.items():
+            freq_map[v].append(ke)
+
+        sorted_freq_map_keys = sorted(freq_map.keys(), reverse=True)
+        
+        # what we have conflicts i.e two or more nums have the same freq?
+        # how do we return the top k here
+        res = []
+        for key in sorted_freq_map_keys:
+            values = freq_map[key]
+            print(f"values = {values}")
+            
+            if k-len(values) >= 0:
+                res += values
+                k = k-len(values)
+
+            # if k>0:
+            #     res += values[:k]
+
+        return res
+
+
+
+                
 
 
         
