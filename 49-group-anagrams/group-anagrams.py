e6d6611583654235c79e81af1ab5428c9c53e12c
Hari Vamsi <harivamsiyadavalli@gmail.com>
2026-01-24 13:28:38 -0800

    Time: 15 ms (49.43%) | Memory: 23.9 MB (11.54%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = []
-        hashmap = {} # count seq -> list of strs
+        res = defaultdict(list)
 
-        for i in range(len(strs)):
-            count = [0] *26
-            s = strs[i]
-            for j in s:
-                count[ord(j) - ord('a')] += 1
-            if tuple(count) in hashmap:
-                hashmap[tuple(count)].append(s)
-            else:
-                hashmap[tuple(count)] = [s]
+        for s in strs:
+            # generate a count print for the s:str
+            count = [0] * 26
+            for c in s:
+                count[ ord(c) - ord('a') ] += 1
+            
+            res[tuple(count)] += [s]
 
-        return list(hashmap.values())
+        return list(res.values())
+
+            
         
+
